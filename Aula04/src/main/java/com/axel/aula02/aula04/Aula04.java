6de7830431d80009ffce70f23e42ae0ad3a60b2f
Axel <axel.selbach@duque.g12.br>
2025-08-25 19:40:31 -0300

    Primeiro commit da aplicação

---
+++ Aula04/src/main/java/com/axel/aula02/aula04/Aula04.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.axel.aula02.aula04;
+
+/**
+ *
+ * @author 08272
+ */
+public class Aula04 {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        int soma = a+b;
+    }
+}
